5ea5ad37e5f1f8dd651a924673724ae307352fbf
Leon Stafford <me@ljs.dev>
2021-09-03 23:36:50 +1000

    rm Gutenberg crap

--- hewbrewarabic-study.php
+++ hewbrewarabic-study.php
@@ -32,3 +32,11 @@ function hebrewarabic_rm_wp_version_number() {
 }
 
 add_filter('the_generator', 'hebrewarabic_rm_wp_version_number');
+
+function hebrewarabic_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+
+add_action( 'wp_enqueue_scripts', 'hebrewarabic_remove_wp_block_library_css', 100 );
