d261835858bd4b70a4565c4d913bd8a4260f5943
Leon Stafford <me@ljs.dev>
2021-09-03 23:28:12 +1000

    initial commit

---
+++ hewbrewarabic-study.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: HebrewArabicStudy
+ * Plugin URI:  https://hebrewarabic.study
+ * Description: Custom functions for HebrewArabic.study
+ * Version:     0.0.1
+ * Author:      Leon Stafford
+ * Author URI:  https://ljs.dev
+ * Text Domain: hebrewarabicstudy
+ *
+ * @package     HebrewArabicStudy    
+ */
+
+// remove all tags from header
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'rest_output_link_wp_head' );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+
+function hebrewarabic_rm_wp_version_number() {
+    return '';
+}
+
+add_filter('the_generator', 'hebrewarabic_rm_wp_version_number');
